006dbc7ea846467023ed1e2ee15ee5b28405fd9e
Jack Chumo <jacqchumo@gmail.com>
2025-03-21 15:40:07 +0300

    changes

--- wave/database/migrations/2024_03_29_225435_create_categories_table.php
+++ wave/database/migrations/2024_03_29_225435_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger('parent_id')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->integer('order')->default(1);
             $table->string('name');
             $table->string('slug')->unique();
