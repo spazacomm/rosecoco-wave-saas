f8070193667383ed43e3ebc54db70b95313caedf
Jack Chumo <jacqchumo@gmail.com>
2025-03-25 16:59:36 +0300

    mautic tracking

--- resources/themes/rosec/components/layouts/marketing.blade.php
+++ resources/themes/rosec/components/layouts/marketing.blade.php
@@ -16,5 +16,15 @@
     @include('theme::partials.footer-scripts')
     {{ $javascript ?? '' }}
     <script src="//ma.rosecoco.co.ke/focus/1.js" type="text/javascript" charset="utf-8" async="async"></script>
+
+    <script>
+    (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+        w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+        m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','http://ma.rosecoco.co.ke/mtc.js','mt');
+
+    mt('send', 'pageview');
+</script>
+
 </body>
 </html>
