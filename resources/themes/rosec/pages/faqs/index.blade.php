fa2c58de3b50fd6d6f420f3fbcf23881f5b0d23f
Jack Chumo <jacqchumo@gmail.com>
2025-03-25 10:53:30 +0300

    added FAQ template

---
+++ resources/themes/rosec/pages/faqs/index.blade.php
@@ -0,0 +1,108 @@
+<?php
+    use function Laravel\Folio\{middleware, name};
+    name('faqs');
+?>
+
+<x-layouts.marketing>
+<x-container class="py-10">
+
+<div class="common-section">
+        <div class="container-small mx-auto px-lg-5">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="fs-4 mb-1 h-fonts f600">How can we help?</h4>
+                    <p class="text-dark-gray-9">Frequently Asked Questions</p>
+                    <div class="accordion" id="accordionExample">
+
+                                                    <div class="accordion-item border-0 border-bottom border-dark-gray-2 bg-transparent rounded-0">
+                                <button class="accordion-button fs-6 py-3 text-white f500 border-0 bg-transparent shadow-none p-0 collapsed" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_0" aria-expanded="false" aria-controls="collapse_0">
+                                    Can I book more than one escort at a time?
+                                </button>
+                                <div id="collapse_0" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body px-md-3 px-0 mb-4">
+                                        <div class="row text-dark-gray-9">
+                                            <p>Of course you can and some of our girls even do duos with each other making this easier for you and this is found here on the&nbsp;<a href="https://www.crushescorts.com/duo-gallery">DUO page</a>, however not all of them offer this service so you&rsquo;d have to look on their profile pages and see if they do, this is also in the information as well if you&rsquo;re more of a reader.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                                                    <div class="accordion-item border-0 border-bottom border-dark-gray-2 bg-transparent rounded-0">
+                                <button class="accordion-button fs-6 py-3 text-white f500 border-0 bg-transparent shadow-none p-0 collapsed" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_1" aria-expanded="false" aria-controls="collapse_1">
+                                    Are the escorts fee’s negotiable?
+                                </button>
+                                <div id="collapse_1" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body px-md-3 px-0 mb-4">
+                                        <div class="row text-dark-gray-9">
+                                            <p>Unfortunately not, as the ladies represent themselves as well as our agency. This is a serious&nbsp;and full-time profession for most of our girls and because they excel in this, which we know you&rsquo;ll find out sooner or later, they&nbsp;negotiate the fees as it&rsquo;s their livelihood. If however, you were taking your London escort out for a longer period of time than discussed then we will have to discuss pricing with you.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                                                    <div class="accordion-item border-0 border-bottom border-dark-gray-2 bg-transparent rounded-0">
+                                <button class="accordion-button fs-6 py-3 text-white f500 border-0 bg-transparent shadow-none p-0 collapsed" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_2" aria-expanded="false" aria-controls="collapse_2">
+                                    Is this all legal?
+                                </button>
+                                <div id="collapse_2" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body px-md-3 px-0 mb-4">
+                                        <div class="row text-dark-gray-9">
+                                            <p>Yes, of course, it is, as you&rsquo;re not paying for sex you&rsquo;re in fact paying for someone to accompany you on whatever adventure you&rsquo;re going to have and to have an excellent time with, this doesn&rsquo;t have to do with sexual activities at all and it is all about companionship. What our girls do with you is kept between you two and no one else. As an agency we maintain discretion and our girls uphold the same standards for maintaining discreet encounters. We also take age verification seriously and have written documentation from each escort, so know you will always be seeing a girl who is over the age of 18.&nbsp;</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                                                    <div class="accordion-item border-0 border-bottom border-dark-gray-2 bg-transparent rounded-0">
+                                <button class="accordion-button fs-6 py-3 text-white f500 border-0 bg-transparent shadow-none p-0 collapsed" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_3" aria-expanded="false" aria-controls="collapse_3">
+                                    Are the pictures on the escort’s profiles genuine?
+                                </button>
+                                <div id="collapse_3" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body px-md-3 px-0 mb-4">
+                                        <div class="row text-dark-gray-9">
+                                            <p>All of our escort&rsquo;s images are real and genuine because we first interview our girls before we put them on our team of gorgeous&nbsp;<a href="https://www.crushescorts.com/">London escorts</a>&nbsp;just to make sure their pictures are genuine. We won&rsquo;t cheat you, unlike some agencies that try to cheat with fake images of girls. We respect our clientele by enforcing a strict nofake image policy, with some escorts even uploading authentic and unfiltered selfies to their profiles.&nbsp;</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                                                    <div class="accordion-item border-0 border-bottom border-dark-gray-2 bg-transparent rounded-0">
+                                <button class="accordion-button fs-6 py-3 text-white f500 border-0 bg-transparent shadow-none p-0 collapsed" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_4" aria-expanded="false" aria-controls="collapse_4">
+                                    Can I contact the agency?
+                                </button>
+                                <div id="collapse_4" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body px-md-3 px-0 mb-4">
+                                        <div class="row text-dark-gray-9">
+                                            <p>Feel free to contact us about any enquiries&nbsp;you may have,&nbsp;<strong>we are open 11 am to 2 am for calls</strong>&nbsp;or you can send us an email and we can get back to you as soon as we can. If you would like to contact Crush Escorts via our website then please visit our&nbsp;contact page.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                                                    <div class="accordion-item border-0 border-bottom border-dark-gray-2 bg-transparent rounded-0">
+                                <button class="accordion-button fs-6 py-3 text-white f500 border-0 bg-transparent shadow-none p-0 collapsed" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapse_5" aria-expanded="false" aria-controls="collapse_5">
+                                    Do you offer DUO escort Services?
+                                </button>
+                                <div id="collapse_5" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body px-md-3 px-0 mb-4">
+                                        <div class="row text-dark-gray-9">
+                                            <p>Yes! We offer plenty of DUO escort services throughout London. Visit our<a href="https://www.crushescorts.com/duo-gallery">&nbsp;DUO escorts</a>&nbsp;page to find our DUO escorts and more information.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                                            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+</x-container>
+</x-layouts.marketing>
