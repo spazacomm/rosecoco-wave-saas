d7c8bda2e2fa85b5ace30f0319a7956297bc39f2
Jack Chumo <jacqchumo@gmail.com>
2025-04-14 20:54:01 +0300

    update paesapal asd a gateway

--- wave/resources/views/livewire/billing/checkout.blade.php
+++ wave/resources/views/livewire/billing/checkout.blade.php
@@ -1,5 +1,5 @@
 <section>
-    <div x-data="{ 
+    <div x-data="{
             billing_cycle_available: @entangle('billing_cycle_available'),
             billing_cycle_selected: @entangle('billing_cycle_selected'),
             toggleButtonClicked(el, month_or_year){
@@ -16,7 +16,7 @@
         }"
         @loader-show.window="fullScreenLoader = true"
         @loader-hide.window="fullScreenLoader = false"
-        @loader-message.window="fullScreenLoaderMessage = event.detail.message" 
+        @loader-message.window="fullScreenLoaderMessage = event.detail.message"
         class="flex items-start justify-center w-full h-full rounded-xl">
         <div class="flex flex-col flex-wrap w-full mx-auto lg:max-w-4xl">
             <x-billing.billing_cycle_toggle></x-billing.billing_cycle_toggle>
@@ -24,14 +24,13 @@
             <div class="h-full space-y-5">
                 @foreach($plans as $plan)
                     @php $features = explode(',', $plan->features); @endphp
-                    <div 
-                        {{--  Say that you have a monthly plan that doesn't have a yearly plan, in that case we will hide the place that doesn't have a price_id --}}
-                        x-show="(billing_cycle_selected == 'month' && '{{ $plan->monthly_price_id }}' != '') || (billing_cycle_selected == 'year' && '{{ $plan->yearly_price_id }}' != '')" 
+                    <div
+                        x-show="(billing_cycle_selected == 'month' && '{{ $plan->monthly_price_id }}' != '') || (billing_cycle_selected == 'year' && '{{ $plan->yearly_price_id }}' != '')"
                         class="w-full max-w-full px-0 mx-auto group">
                         <div class="flex flex-col mb-10 h-full bg-white dark:bg-neutral-800 rounded-xl ease-out duration-300 border-2 border-gray-200 dark:border-neutral-700 shadow-sm sm:mb-0 group-hover:border-{{ config('devdojo.billing.style.color') }}-500">
                             <div class="p-6 lg:p-8">
                                 <div class="relative text-gray-500 dark:text-neutral-400">
-                                    <span lass="text-lg md:text-xl font-semibold rounded-full">
+                                    <span class="text-lg md:text-xl font-semibold rounded-full">
                                         {{ $plan->name }} Plan
                                         {{ $billing_cycle_selected }}
                                     </span>
@@ -65,22 +64,29 @@
                                             <x-billing.button wire:click="redirectToStripeCheckout('{{ $plan->id }}')" wire:target="redirectToPaymentProvider" rounded="md" color="{{ config('devdojo.billing.style.color') }}">
                                                 Subscribe to this Plan
                                             </x-billing.button>
+                                        @elseif(config('wave.billing_provider') == 'pesapal')
+                                            <x-billing.button x-on:click="
+                                                    if(billing_cycle_selected == 'month'){ redirectToPesapal('{{ $plan->monthly_price_id }}'); }
+                                                    if(billing_cycle_selected == 'year'){ redirectToPesapal('{{ $plan->yearly_price_id }}'); }
+                                                "
+                                                rounded="md" color="{{ config('devdojo.billing.style.color') }}">
+                                                Subscribe to this Plan
+                                            </x-billing.button>
                                         @else
                                             @if($change)
-
                                                 <x-filament::modal width="lg" id="change-plan-modal">
                                                     <x-slot name="trigger">
-                                                            @if($plan->id == $userPlan->id)
-                                                                <x-billing.button rounded="md" color="{{ config('devdojo.billing.style.color') }}" x-show="billing_cycle_selected == '{{ $userSubscription->cycle }}'">You are currently on this plan</x-billing.button>
-                                                                <x-billing.button rounded="md" color="{{ config('devdojo.billing.style.color') }}" x-show="billing_cycle_selected != '{{ $userSubscription->cycle }}'">Switch to this plan</x-billing.button>
-                                                            @else
-                                                                <x-billing.button rounded="md" color="{{ config('devdojo.billing.style.color') }}">Switch to this plan</x-billing.button>
-                                                            @endif
+                                                        @if($plan->id == $userPlan->id)
+                                                            <x-billing.button rounded="md" color="{{ config('devdojo.billing.style.color') }}" x-show="billing_cycle_selected == '{{ $userSubscription->cycle }}'">You are currently on this plan</x-billing.button>
+                                                            <x-billing.button rounded="md" color="{{ config('devdojo.billing.style.color') }}" x-show="billing_cycle_selected != '{{ $userSubscription->cycle }}'">Switch to this plan</x-billing.button>
+                                                        @else
+                                                            <x-billing.button rounded="md" color="{{ config('devdojo.billing.style.color') }}">Switch to this plan</x-billing.button>
+                                                        @endif
                                                     </x-slot>
                                                     <div x-data class="relative flex flex-col items-center justify-center">
-                                                        <div 
+                                                        <div
                                                             @if($plan->id == $userPlan->id)
-                                                                x-show="billing_cycle_selected != '{{ $userSubscription->cycle }}'" 
+                                                                x-show="billing_cycle_selected != '{{ $userSubscription->cycle }}'"
                                                             @endif class="relative flex flex-col items-center justify-center w-full">
                                                             <div class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-blue-100 rounded-full sm:mx-0 sm:h-10 sm:w-10">
                                                                 <svg class="w-6 h-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"/><circle cx="128" cy="120" r="40" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><path d="M63.8,199.37a72,72,0,0,1,128.4,0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><polyline points="200 128 224 152 248 128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><polyline points="8 128 32 104 56 128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><path d="M32,104v24a96,96,0,0,0,174,56" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><path d="M224,152V128A96,96,0,0,0,50,72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>
@@ -92,27 +98,23 @@
                                                                 </div>
                                                             </div>
                                                             <div class="relative flex items-center w-full space-x-3">
-                                                                <x-button x-on:click="$dispatch('close-modal', { id: 'change-plan-modal' })" color="secondary" class="w-1/2">Cancel</x-button> 
+                                                                <x-button x-on:click="$dispatch('close-modal', { id: 'change-plan-modal' })" color="secondary" class="w-1/2">Cancel</x-button>
                                                                 <x-button wire:click="switchPlan('{{ $plan->id }}')" color="info" class="w-1/2">Yes, Switch Plans</x-button>
                                                             </div>
                                                         </div>
-                                                        <div 
+                                                        <div
                                                             x-show="billing_cycle_selected == '{{ $userSubscription->cycle }}' && {{ ($plan->id == $userPlan->id) ? 'true' : 'false' }}"
                                                             class="flex items-center justify-center">
                                                             <p>You are currently on this plan</p>
                                                         </div>
                                                     </div>
-                                                    {{-- Modal content --}}
                                                 </x-filament::modal>
-
-                                                
                                             @else
                                                 <x-billing.button x-on:click="
-                                                        if(billing_cycle_selected == 'month'){ openCheckout('{{ $plan->monthly_price_id }}'); }
-                                                        if(billing_cycle_selected == 'year'){ openCheckout('{{ $plan->yearly_price_id }}'); }
-                                                    " 
-                                                    rounded="md" color="{{ config('devdojo.billing.style.color') }}"
-                                                >
+                                                        if(billing_cycle_selected == 'month'){ redirectToPesapal('{{ $plan->monthly_price_id }}'); }
+                                                        if(billing_cycle_selected == 'year'){ redirectToPesapal('{{ $plan->yearly_price_id }}'); }
+                                                    "
+                                                    rounded="md" color="{{ config('devdojo.billing.style.color') }}">
                                                     Subscribe to this Plan
                                                 </x-billing.button>
                                             @endif
@@ -138,7 +140,6 @@
             window.paddle_public_key = '{{ config("wave.paddle.public_key") }}';
 
             window.injectPaddleCDN = function(){
-                // only inject if the Paddle object is undefined
                 if (typeof Paddle == 'undefined') {
                     const script = document.createElement('script');
                     script.src = 'https://cdn.paddle.com/paddle/v2/paddle.js';
@@ -190,9 +191,9 @@
             }
 
             window.closeLoader = function(){
-               window.dispatchEvent(new CustomEvent('loader-hide')); 
+               window.dispatchEvent(new CustomEvent('loader-hide'));
             }
-            
+
             window.openCheckout = function(priceId) {
                 if(paddle_public_key){
                     Paddle.Checkout.open({
@@ -219,10 +220,15 @@
             document.addEventListener('DOMContentLoaded', function() {
                 injectPaddleCDN();
                 whenPaddleIsReady(function(){
-                    
                     initialize();
                 });
             });
+        </script>
+    @elseif(config('wave.billing_provider') == 'pesapal')
+        <script>
+            window.redirectToPesapal = function(priceId) {
+                window.location.href = '{{ url("pesapal/checkout") }}/' + priceId;
+            }
         </script>
     @endif
 </section>
