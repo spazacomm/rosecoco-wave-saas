3d0ac68663013a2743752cf6bd279b8388bd25d1
Jack Chumo <jacqchumo@gmail.com>
2025-04-28 20:01:37 +0300

    gogole tag

--- resources/themes/rosec/components/layouts/marketing.blade.php
+++ resources/themes/rosec/components/layouts/marketing.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P7W5YK22Q5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P7W5YK22Q5');
+</script>
+
     @include('theme::partials.head', ['seo' => ($seo ?? null) ])
 </head>
 <body x-data class="bg-main-bg @if($bodyClass ?? false){{ $bodyClass }}@endif" x-cloak>
